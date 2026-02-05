9148abde66fd44060ad86523915f85e51c2779a4
Yuchi Hsu <yuchihsu@stanford.edu>
2026-02-05 15:14:58 -0800

    chore: exp setup

---
+++ README.md
@@ -0,0 +1,12 @@
+# CS217 Final Project
+
+# ML Experiments Setup
+
+We use `uv` to manage the environment.
+
+## Setup
+
+```bash
+uv venv
+uv sync
+```
---
+++ experiments/main.py
@@ -0,0 +1,13 @@
+"""
+Main entry point for the experiments.
+"""
+
+def main():
+    """
+    Main entry point for the experiments.
+    """
+    print("Hello from cs217-final-project!")
+
+
+if __name__ == "__main__":
+    main()
