88033cc5a44663887917b7a7c3afd17bf52bdd2f
ba144220 <b08901097@ntu.edu.tw>
2026-02-08 03:44:26 +0800

    feat: load hf model

--- experiments/main.py
+++ experiments/main.py
@@ -3,13 +3,32 @@ Main entry point for the experiments.
 """
 
 import lm_eval
+from lm_eval.models.huggingface import HFLM
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+MODEL_NAME = "Qwen/Qwen3-1.7B"
+
 def main():
     """
     Main entry point for the experiments.
     """
+    model = AutoModelForCausalLM.from_pretrained(
+        MODEL_NAME, 
+        device_map="auto", 
+        dtype=torch.bfloat16,
+        tie_word_embeddings=False
+    )
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    lm = HFLM(
+        pretrained=model,
+        tokenizer=tokenizer,
+        device="cuda",
+        dtype=torch.bfloat16,
+    )
+
     results = lm_eval.simple_evaluate(
-        model="hf",
-        model_args="pretrained=Qwen/Qwen3-1.7B,device=cuda,dtype=bfloat16",
+        model=lm,
         tasks=["hellaswag"],
         limit=1000,
     )
